341b241f80793478ab7f7f63405077afd61d50a9
jkkachi <jkkachi@gmail.com>
2025-11-24 15:29:45 +0900

    스터디4

---
+++ classes_fourcls.py
@@ -0,0 +1,31 @@
+
+
+
+#        # 메서드(함수)
+class fourcal:
+
+first=0
+second=0
+
+def _init_(self):
+    self,first=4
+    self.second=6
+    pass
+def add( self,a, b):
+    return a + b
+def sub(self, a, b):
+    return a - b
+def mul( a, b):
+    return a * b
+def div( a, b):
+    if b == 0:
+        return "0으로 나눌 수 없습니다."
+    return a / b
+
+
+if__name__ == "__main__"
+fourcal=fourcal()
+print(f"3+3={fourcal.add(3,5)}")
+print(f"10-4={fourcal.sub(10,4)}")
+print(f"7*8={fourcal.mul(7,8)}")
+print(f"20/5={fourcal.div(20,5)}")
--- codes/08_functions.py
+++
@@ -1,9 +0,0 @@
-temp1 = 77
-celsius1 = (temp1 - 32) * 5 / 9
-print(celsius1)
-temp2 = 95
-celsius2 = (temp2 - 32) * 5 / 9
-print(celsius2)
-temp3 = 50
-celsius3 = (temp3 - 32) * 5 / 9
-print(celsius3)
